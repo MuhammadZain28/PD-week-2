8db91dd217bc12165c6af4c84aad751bf108e129
MuhammadZain28 <zaina.azhar2005@gmail.com>
2024-09-14 06:51:45 -0700

    Add files via upload

---
+++ animal.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 87");
+cout<<"----------------------------------- \n"<<endl;
+
+cout<<"        o     ^__^                  \n"<<endl;
+
+cout<<"         o    (OO)\\__________      \n"<<endl;
+
+cout<<"              (__)\\          )\\/\\\n";
+cout<<"                  ||------w   |     \n";
+cout<<"                  ||         ||     \n";
+}
---
+++ gun.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 09");
+cout<<" +--^-----------,--------,-------,-----^-,  \n";
+cout<<"  | |||||||||    '---------'      |       O \n";
+cout<<"  '+--------------------------------------| \n";
+cout<<"    ',----------,---------,-------------' \n";
+cout<<"      / XXXXXXX /'|       /' \n";
+cout<<"     / XXXXXXX /  |      /' \n";
+cout<<"    / XXXXXXX /'--------' \n";
+cout<<"   / XXXXXXX / \n";
+cout<<"  / XXXXXXX / \n";
+cout<<" C_________C \n";
+cout<<"  '-------' \n";
+}
